d89a034e42e1831093eb5bdec0cce6ad17eb58ef
Kennedy Njeri <mistakenz123@gmail.com>
2019-04-03 20:23:12 +0300

    configuration of pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from .views import index, blog
+from .views import index, blog, post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('blog/', blog, name='blog'),
+    path('post/', post, name='post'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
